f69978d17b6389fed91311e61921d28e08f2cf3b
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-02-22 08:38:42 +0100

    when to use

--- creational/factoryMethod.js
+++ creational/factoryMethod.js
@@ -51,3 +51,6 @@ devManager.takeInterview() // Output: Asking about design patterns
 
 const marketingManager = new MarketingManager()
 marketingManager.takeInterview() // Output: Asking about community building.
+
+// When to use
+// Useful when there is some generic processing in a class but the required sub-class is dynamically decided at runtime. Or putting it in other words, when the client doesn't know what exact sub-class it might need.
