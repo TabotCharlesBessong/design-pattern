8344ff3cae43591160a831ec8d928ea4874e4950
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-12-06 05:24:39 +0100

    demonstration of the problem without the template pattern

---
+++ template/demo/GenerateReportTask.java
@@ -0,0 +1,15 @@
+package template.demo;
+
+public class GenerateReportTask {
+  private AuditTrail auditTrail;
+
+  public GenerateReportTask(AuditTrail auditTrail) {
+    this.auditTrail = auditTrail;
+  }
+
+  public void execute() {
+    auditTrail.record();
+
+    System.out.println("Generate Report");
+  }
+}
