ddcf1169a0bd9c8b7cdddaf881d7fe8dfb5f1c23
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-12-07 04:29:02 +0100

    implemented a text editor using the command design pattern

--- command/Main.java
+++ command/Main.java
@@ -1,14 +1,29 @@
 package command;
 
+import command.editor.BoldCommand;
+import command.editor.History;
+import command.editor.HtmlDocument;
+import command.editor.UndoCommand;
 import command.fx.Button;
 
 public class Main {
   public static void main(String[] args){
-    // System.out.println("Starting");
 
     var service = new CustomerService();
     var command = new AddCustomerCommand(service);
     var button = new Button(command);
     button.click();
+
+    var history = new History();
+    var document = new HtmlDocument();
+    document.setContent("Hello world!");
+
+    var boldCommand = new BoldCommand(document,history);
+    boldCommand.execute();
+    System.out.println(document.getContent());
+
+    var undoCommand = new UndoCommand(history);
+    undoCommand.execute();
+    System.out.println(document.getContent());
   }
 }
